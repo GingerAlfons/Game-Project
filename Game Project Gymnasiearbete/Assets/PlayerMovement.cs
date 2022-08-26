81737e73550a1eea6c83abdefab46c41c84f09d3
GingerAlfons <Jonathan.solglantan@live.se>
2022-08-26 13:26:48 +0200

    Added Player and PlayerMovement

---
+++ Game Project Gymnasiearbete/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 2f;
+
+    [Header("Jump")]
+    public LayerMask ground;
+    public float jumpStrength = 15f;
+    public Vector2 boxSize = new Vector2(1f, 1f);
+    public Vector3 offset = new Vector3(1f, 0f, 0f);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        bool grounded = Physics2D.OverlapBox(transform.position + offset, boxSize, 0f);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (grounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+    }
+}
