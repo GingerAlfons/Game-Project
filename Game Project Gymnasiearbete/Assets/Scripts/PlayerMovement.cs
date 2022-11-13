25077643effb27decfe3edec3e7bd89e771ec573
Wokken <ehwokke@outlook.com>
2022-11-13 13:34:14 +0100

    Dubbelhopp
    Lade till en variabel float dubbleJump och gjorde så man kan dubbelhoppa

--- Game Project Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
+++ Game Project Gymnasiearbete/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public CapsuleCollider2D cc;
     public float speed = 2f;
     public KeyCode JumpButton;
     public KeyCode LeftButton;
     public KeyCode RightButton;
+    public KeyCode DuckButton;
     float HorizontalInput = 0f;
 
     [Header("Jump")]
     public LayerMask ground;
     public float jumpStrength = 15f;
+    public float dubbleJump;
     public Vector2 boxSize = new Vector2(1f, 1f);
     public Vector3 offset = new Vector3(0f, 0f, 0f);
 
@@ -43,23 +46,34 @@ public class PlayerMovement : MonoBehaviour
         {
             HorizontalInput = 0f;
         }
+     
         bool grounded = Physics2D.OverlapBox(transform.position + offset, boxSize, 0f, ground);
         rb.velocity = new Vector2(HorizontalInput * speed, rb.velocity.y);
 
-        if (grounded && Input.GetKeyDown(JumpButton))
+        if (grounded)
+        {
+            dubbleJump = 2f;
+        }
+
+        if (dubbleJump > 0 && Input.GetKeyDown(JumpButton))
+        {
             Jump();
-
-        //Flippar spriten beroende på vilket håll man kollar åt
-
+        }
     }
 
     public void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+        dubbleJump--;
     }
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireCube(transform.position + offset, boxSize);
+    }
+
+    private void Duck()
+    {
+
     }
 }
