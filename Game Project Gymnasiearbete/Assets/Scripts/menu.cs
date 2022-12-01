5221abf5b5f7d2a58b153144eb1b7474557f3ded
SauceChihuahua <robinpettersson10@hotmail.com>
2022-12-01 09:58:05 +0100

    meny
    ok så main och options är i samma scen nu

--- Game Project Gymnasiearbete/Assets/Scripts/menu.cs
+++ Game Project Gymnasiearbete/Assets/Scripts/menu.cs
@@ -7,14 +7,6 @@ public class menu : MonoBehaviour
 {
     public void play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-    public void back()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-    public void options()
-    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void quit()
