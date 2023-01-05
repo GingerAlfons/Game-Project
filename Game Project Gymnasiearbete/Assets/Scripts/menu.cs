aa97b74acde2f165e8535fd538a21f913025578b
SauceChihuahua <robinpettersson10@hotmail.com>
2023-01-05 16:02:58 +0100

    Meny grejer
    Ok så du kan faktiskt pausa nu (esc). Allt i menyerna fungerar förutom volume slidern men det fixar jag sen när vi har audio.

--- Game Project Gymnasiearbete/Assets/Scripts/menu.cs
+++ Game Project Gymnasiearbete/Assets/Scripts/menu.cs
@@ -5,6 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
     public void play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
