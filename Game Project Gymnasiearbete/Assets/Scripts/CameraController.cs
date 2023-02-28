237891f886dd76046b3e22092ea20f2c050be29a
GingerAlfons <Jonathan.solglantan@live.se>
2023-02-28 13:53:44 +0100

    + max x och min x gränser för kameran

--- Game Project Gymnasiearbete/Assets/Scripts/CameraController.cs
+++ Game Project Gymnasiearbete/Assets/Scripts/CameraController.cs
@@ -7,27 +7,35 @@ public class CameraController : MonoBehaviour
     public Rigidbody2D rbCamera;
     public Transform redPlayerPos;
     public Transform greenPlayerPos;
+    public Transform cameraPos;
+    public float maxXConstant;
     public float minYConstant;
     public float moveSpeed = 1f;
-    // Har inte använt denna något: public float acc = 0.1f;
     public float moveDirection;
     public float moveCooldown = 0f;
-    void Start()
-    {
-        
-    }
     
     void Update()
     {
         //Uppdaterar y-värdet enligt funktionen
         SetYValue();
 
+        //Kollar om kameran håller på att åka ut ur mappen
+        if ((cameraPos.position.x - maxXConstant) <= -60)
+        {
+            FixCameraMovement(1);
+        }
+        else if ((cameraPos.position.x + maxXConstant) >= 60)
+        {
+            FixCameraMovement(-1);
+        }
+
         //Kollar om det är dags att kalla move funktionen annars minskar timern
         if (GameManager.Instance.startTimer <= 0)
         {
+            //Kollar om det är dags att flytta kameran
             if (moveCooldown <= 0)
             {
-                move();
+                Move();
                 Debug.Log("Kallar move funktionen");
             }
             else
@@ -53,11 +61,11 @@ public class CameraController : MonoBehaviour
     }
 
     //Move funktionen
-    public void move()
+    public void Move()
     {
         //Slumpar värdet på moveCooldown, moveSpeed och Riktningen
         moveCooldown = Random.Range(2, 5);
-        moveSpeed = Random.Range(1, 5);
+        moveSpeed = Random.Range(2, 6);
         int rand = Random.Range(1, 3);
 
         if (rand == 1)
@@ -71,5 +79,16 @@ public class CameraController : MonoBehaviour
 
         //Ger kameran en velocity i x-led
         rbCamera.velocity = new Vector2(moveDirection * moveSpeed, 0f);
+    }
+
+    //Ändrar riktningen på kameran om den håller på att åka ut ur mappen
+    public void FixCameraMovement(int moveCorrection)
+    {
+        Debug.Log("Förhindrar kameran från att åka ut ur mappen");
+        moveCooldown = 8f;
+        moveSpeed = Random.Range(2, 6);
+
+        //Ger kameran en velocity i x-led
+        rbCamera.velocity = new Vector2(moveCorrection * moveSpeed, 0f);
     }
 }
